dae60794f19d87b61e7688b83f85f47f313e424c
MD RASID ANSARI <142312944+MdRasidAnsari@users.noreply.github.com>
2024-03-28 16:45:59 +0530

    Add files via upload

---
+++ Weather_App_1/index.js
@@ -0,0 +1,29 @@
+var inputvalue = document.querySelector('#cityinput')
+var btn = document.querySelector('#add')
+var city = document.querySelector('#cityoutput')
+var descrip = document.querySelector('#description')
+var temp = document.querySelector('#temp')
+var wind = document.querySelector('#wind')
+apik = "28f154ab40a69dca52fafd5396318d52";
+
+function convertion(val) {
+  return (val - 273).toFixed(3)
+}
+
+btn.addEventListener('click', function () {
+  fetch('https://api.openweathermap.org/data/2.5/weather?q=' + inputvalue.value + '&appid=' + apik).then(res => res.json())
+
+    .then(data => {
+      var nameval = data['name']
+      var descrip = data['weather']['0']['description']
+      var temprature = data['main']['temp']
+      var windspeed = data['wind']['speed']
+
+      city.innerHTML = `Weather of <span>${nameval}<span>`
+      temp.innerHTML = `Teamperature: <span>${ convertion(temprature)} Degree Celsius </span>`;
+      description.innerHTML = `Sky Condition : <span> ${ descrip} <span>`
+      wind.innerHTML = `Wind Speed: <span>${windspeed} Km/h <span> `
+    }
+    )
+    .catch(err => alert('You entered Worng city name'))
+})
